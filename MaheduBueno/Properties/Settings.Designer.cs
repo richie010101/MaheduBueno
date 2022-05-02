09a8f448177c59cbccd88d7c5c04f61c501dd7b7
richie010101 <ricardojesusreyes1@gmail.com>
2022-05-02 09:23:14 -0500

    base de datos

--- MaheduBueno/Properties/Settings.Designer.cs
+++ MaheduBueno/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MaheduBueno.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,6 +26,7 @@ namespace MaheduBueno.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("server=localhost;user id=root;port=3308;database=mahedubueno")]
         public string Valor {
             get {
                 return ((string)(this["Valor"]));
