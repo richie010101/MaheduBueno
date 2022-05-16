a8f0d487e0aeb9ec51f6a9c031af61f70d538223
NEREIDA GARCIA AGUIÑIGA <84287483+NEREIDAGARCIAA@users.noreply.github.com>
2022-05-16 14:00:51 -0500

    insertar usuario

--- MaheduBueno/MenuAgregarUsuario.Designer.cs
+++ MaheduBueno/MenuAgregarUsuario.Designer.cs
@@ -40,12 +40,14 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.NombreU = new System.Windows.Forms.TextBox();
+            this.ApellidoP = new System.Windows.Forms.TextBox();
+            this.Contraseña = new System.Windows.Forms.TextBox();
+            this.ConfirContra = new System.Windows.Forms.TextBox();
+            this.TipoUsuario = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.ApellidoM = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -149,9 +151,9 @@
             this.label5.ForeColor = System.Drawing.Color.White;
             this.label5.Location = new System.Drawing.Point(42, 232);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(89, 22);
+            this.label5.Size = new System.Drawing.Size(146, 22);
             this.label5.TabIndex = 14;
-            this.label5.Text = "Apellidos";
+            this.label5.Text = "Apellido Paterno";
             // 
             // label6
             // 
@@ -159,7 +161,7 @@
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(42, 274);
+            this.label6.Location = new System.Drawing.Point(42, 347);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(99, 22);
             this.label6.TabIndex = 15;
@@ -171,7 +173,7 @@
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(42, 322);
+            this.label7.Location = new System.Drawing.Point(42, 389);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(181, 22);
             this.label7.TabIndex = 16;
@@ -183,72 +185,94 @@
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(42, 377);
+            this.label8.Location = new System.Drawing.Point(42, 436);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(134, 22);
             this.label8.TabIndex = 17;
             this.label8.Text = "Tipo de usuario";
             // 
-            // textBox1
+            // NombreU
             // 
-            this.textBox1.Location = new System.Drawing.Point(240, 187);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(373, 22);
-            this.textBox1.TabIndex = 18;
-            this.textBox1.Text = "Mariana";
+            this.NombreU.Location = new System.Drawing.Point(240, 187);
+            this.NombreU.Name = "NombreU";
+            this.NombreU.Size = new System.Drawing.Size(373, 22);
+            this.NombreU.TabIndex = 18;
+            this.NombreU.Text = "Mariana";
             // 
-            // textBox2
+            // ApellidoP
             // 
-            this.textBox2.Location = new System.Drawing.Point(240, 233);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(373, 22);
-            this.textBox2.TabIndex = 19;
+            this.ApellidoP.Location = new System.Drawing.Point(240, 233);
+            this.ApellidoP.Name = "ApellidoP";
+            this.ApellidoP.Size = new System.Drawing.Size(373, 22);
+            this.ApellidoP.TabIndex = 19;
             // 
-            // textBox3
+            // Contraseña
             // 
-            this.textBox3.Location = new System.Drawing.Point(240, 275);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(373, 22);
-            this.textBox3.TabIndex = 20;
-            this.textBox3.Text = "***********";
+            this.Contraseña.Location = new System.Drawing.Point(240, 348);
+            this.Contraseña.Name = "Contraseña";
+            this.Contraseña.Size = new System.Drawing.Size(373, 22);
+            this.Contraseña.TabIndex = 20;
+            this.Contraseña.Text = "***********";
             // 
-            // textBox4
+            // ConfirContra
             // 
-            this.textBox4.Location = new System.Drawing.Point(240, 323);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(373, 22);
-            this.textBox4.TabIndex = 21;
-            this.textBox4.Text = "**********";
+            this.ConfirContra.Location = new System.Drawing.Point(240, 389);
+            this.ConfirContra.Name = "ConfirContra";
+            this.ConfirContra.Size = new System.Drawing.Size(373, 22);
+            this.ConfirContra.TabIndex = 21;
+            this.ConfirContra.Text = "**********";
             // 
-            // textBox5
+            // TipoUsuario
             // 
-            this.textBox5.Location = new System.Drawing.Point(240, 377);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(373, 22);
-            this.textBox5.TabIndex = 22;
-            this.textBox5.Text = "Administrador";
+            this.TipoUsuario.Location = new System.Drawing.Point(240, 436);
+            this.TipoUsuario.Name = "TipoUsuario";
+            this.TipoUsuario.Size = new System.Drawing.Size(373, 22);
+            this.TipoUsuario.TabIndex = 22;
+            this.TipoUsuario.Text = "Administrador";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(324, 426);
+            this.button3.Location = new System.Drawing.Point(315, 474);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(130, 33);
             this.button3.TabIndex = 23;
             this.button3.Text = "Guardar Usuario";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(41, 291);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(152, 22);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Apellido Materno";
+            // 
+            // ApellidoM
+            // 
+            this.ApellidoM.Location = new System.Drawing.Point(240, 291);
+            this.ApellidoM.Name = "ApellidoM";
+            this.ApellidoM.Size = new System.Drawing.Size(373, 22);
+            this.ApellidoM.TabIndex = 25;
             // 
             // MenuAgregarUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::MaheduBueno.Properties.Resources.fondo2;
-            this.ClientSize = new System.Drawing.Size(800, 495);
+            this.ClientSize = new System.Drawing.Size(800, 519);
+            this.Controls.Add(this.ApellidoM);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TipoUsuario);
+            this.Controls.Add(this.ConfirContra);
+            this.Controls.Add(this.Contraseña);
+            this.Controls.Add(this.ApellidoP);
+            this.Controls.Add(this.NombreU);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -284,11 +308,13 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox NombreU;
+        private System.Windows.Forms.TextBox ApellidoP;
+        private System.Windows.Forms.TextBox Contraseña;
+        private System.Windows.Forms.TextBox ConfirContra;
+        private System.Windows.Forms.TextBox TipoUsuario;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox ApellidoM;
     }
 }
--- MaheduBueno/MenuAgregarUsuario.cs
+++ MaheduBueno/MenuAgregarUsuario.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,33 @@ namespace MaheduBueno
             MenuPrincipal menu = new MenuPrincipal();
 
             menu.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(Properties.Settings.Default.conexion1);
+                String qery = "insert into Nombres, ApellidoP, ApellidoM, UserName, Contraseña, tipoUsuario_idtipoUsuario from mahedu.usuario where " + NombreU.Text + ApellidoP + ApellidoM.Text + ;
+                Console.WriteLine(qery);
+                SqlDataAdapter ada = new SqlDataAdapter(qery, con);
+
+                con.Open();
+
+                DataSet data = new DataSet();
+
+                ada.Fill(data, "producto");
+
+                dataGridView1.DataSource = data;
+                dataGridView1.DataMember = "producto";
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error en la conexion del servidor busque ayuda" + ex);
+            }
         }
     }
 }
