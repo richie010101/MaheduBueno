bccf12f326012367ff42ede7d014770ff55a79fa
Mjosey21 <joseymike@outlook.com>
2022-05-27 18:10:03 -0500

    Pequenos cambios

--- MaheduBueno/Estadisticas.cs
+++ MaheduBueno/Estadisticas.cs
@@ -29,6 +29,7 @@ namespace MaheduBueno
             recuperarUtilidades();
             masVendidos();
             cambiaDescripcion();
+            cargar();
 
             label2.Text = usuario.username;
         }
@@ -469,21 +470,7 @@ namespace MaheduBueno
                 i--;
             }
         }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            String sql = "select TOP 5 YEAR(A.Fecha) as 'AÑO', sum(C.Precio*B.Cantidad) as 'total ventas por año ' " +
-                "from mahedu.venta as A inner join mahedu.producto_has_venta as B on " +
-                "A.idVenta = B.Venta_idVenta inner join  mahedu.producto as C on B.Producto_idProducto = C.idProducto " +
-                "inner join  mahedu.usuario as D on D.idUsuario = A.Usuario_idUsuario " +
-                "GROUP BY  YEAR(A.Fecha) ORDER BY  YEAR(A.Fecha) DESC";
-
-            DataTable estadistica = EjecutaQuery(sql);
-            int i = estadistica.Rows.Count - 1;
-            chart1.Series["Series1"].Points.Clear();
-            chart2.Series["Series1"].Points.Clear();
-        }
-
+ 
         private void button5_Click(object sender, EventArgs e)
         {
             String sql = "select TOP 12 MONTH(A.Fecha) as 'MES', sum(C.Precio*B.Cantidad) as 'total ventas por mes ' " +
@@ -527,6 +514,7 @@ namespace MaheduBueno
             int c = r.Width / 2;
             // Establecemos la nueva posición del control Label.
             label2.Location = new Point(c - label2.Width / 2, label2.Location.Y);
+            label1.Location = new Point(c - label1.Width / 2, label1.Location.Y);
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -600,6 +588,117 @@ namespace MaheduBueno
                 estadistica = EjecutaQuery(sql);
                 textDescripcion.Text += "Cantidad de productos vendidos -------" + estadistica.Rows[0][1].ToString() + "\n";
 
+            }
+        }
+
+        public void cargar()
+        {
+            //Query a ejecutar, solicita el total de ventas realizadas en los ultimos 7 dias
+            String sql = "select TOP 7 DAY(A.Fecha) AS 'DIA', MONTH(A.Fecha) as 'MES', sum(C.Precio*B.Cantidad) as 'total ventas por dia ' " +
+                "from mahedu.venta as A inner join mahedu.producto_has_venta as B on " +
+                "A.idVenta = B.Venta_idVenta inner join  mahedu.producto as C on B.Producto_idProducto = C.idProducto " +
+                "inner join  mahedu.usuario as D on D.idUsuario = A.Usuario_idUsuario " +
+                "GROUP BY  YEAR(A.Fecha) ,MONTH(A.Fecha) , DAY(A.Fecha) ORDER BY  YEAR(A.Fecha) DESC,'MES' DESC ,'DIA' DESC";
+
+            //Se crea una tabla y se utiliza el metodo para ejecutar el query
+            DataTable estadistica = EjecutaQuery(sql);
+            int i = estadistica.Rows.Count - 1;
+            chart1.Series["Series1"].Points.Clear();
+            chart2.Series["Series1"].Points.Clear();
+
+
+            int[] dias = new int[7];
+            float[] ventas = new float[7];
+            int[] mes = new int[7];
+
+
+            int dia = int.Parse(estadistica.Rows[0][0].ToString());
+            dias[0] = int.Parse(estadistica.Rows[0][0].ToString());
+            mes[0] = int.Parse(estadistica.Rows[0][1].ToString());
+            ventas[0] = float.Parse(estadistica.Rows[0][2].ToString());
+            int mesactual = int.Parse(estadistica.Rows[0][1].ToString());
+
+
+
+            int j;
+            int R = 1;
+            for (j = 1; j < 7; j++)
+            {
+                dia--;
+
+                if (dia == 0)
+                {
+                    if (mesactual == 2 || mesactual == 4 || mesactual == 6 || mesactual == 8 || mesactual == 9 || mesactual == 11 || mesactual == 1)
+                    {
+                        dia = 31;
+                        if (mesactual != 1)
+                        {
+                            mesactual = mesactual - 1;
+                        }
+                        else
+                        {
+                            mesactual = 12;
+                        }
+
+                    }
+                    else if (mesactual == 5 || mesactual == 7 || mesactual == 10 || mesactual == 12)
+                    {
+                        dia = 30;
+                        mesactual--;
+                    }
+                    else
+                    {
+                        dia = 28;
+                        mesactual--;
+                    }
+
+
+                }
+                /*
+                 *    Meses con 30 días: Abril, Junio, Septiembre y Noviembre.
+                      Meses con 31 días: Enero, Marzo, Mayo, Julio, Agosto, Octubre y Diciembre.
+                      Meses con 28 días: Febrero (Menos cuando es bisiesto que tiene 29 días).
+                 * 
+                 * */
+
+
+                for (R = 1; R < 7; R++)
+                {
+
+                    Console.WriteLine("Dia del int: " + dia);
+                    Console.WriteLine("dia a comparar " + estadistica.Rows[R][0].ToString());
+
+                    if (int.Parse(estadistica.Rows[R][0].ToString()) == dia & int.Parse(estadistica.Rows[R][1].ToString()) == mesactual)
+                    {
+                        dias[j] = int.Parse(estadistica.Rows[R][0].ToString());
+                        ventas[j] = float.Parse(estadistica.Rows[R][2].ToString());
+                        mes[j] = mesactual;
+
+
+                        break;
+                    }
+                    else if (R == 6)
+                    {
+
+                        dias[j] = dia;
+                        ventas[j] = 0;
+                        mes[j] = mesactual;
+                        break;
+
+                    }
+
+                }
+            }
+
+
+            float total = 0;
+
+            while (i >= 0)
+            {
+                total += ventas[i];
+                chart1.Series["Series1"].Points.AddXY(mes[i] + "/" + dias[i], ventas[i]);
+                chart2.Series["Series1"].Points.AddXY(mes[i] + "/" + dias[i], total);
+                i--;
             }
         }
     }
